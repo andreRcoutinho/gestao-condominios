7d1939ee6a1dc4b04888d31f9213e5e97a603e98
Francisco Saraiva <francisco_saraiva94@hotmail.com>
2020-04-01 12:42:27 +0100

    Model ServiceType done.

--- backend/src/models/service_type.ts
+++ backend/src/models/service_type.ts
@@ -1,12 +1,31 @@
-import { BaseEntity } from 'typeorm';
+import { BaseEntity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 export class ServiceType extends BaseEntity {
 
+    @PrimaryGeneratedColumn({ name: 'id' })
     private id: Number;
+
+    @Column({ name: 'service_type' })
     private service_type: String;
 
     constructor() {
         super();
+    }
+
+    public getId(): Number {
+        return this.id;
+    }
+
+    public setId(id: Number): void {
+        this.id = id;
+    }
+
+    public getService_type(): String {
+        return this.service_type;
+    }
+
+    public setService_type(service_type: String): void {
+        this.service_type = service_type;
     }
 
 }

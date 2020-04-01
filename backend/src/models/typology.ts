a5bf26ff5b22339f785092de32402dbd9ddb37c5
Francisco Saraiva <francisco_saraiva94@hotmail.com>
2020-04-01 11:01:26 +0100

    Typology model done.

--- backend/src/models/typology.ts
+++ backend/src/models/typology.ts
@@ -1,12 +1,31 @@
-import { BaseEntity } from 'typeorm';
+import { BaseEntity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 export class Typology extends BaseEntity {
 
+    @PrimaryGeneratedColumn({ name: 'id' })
     private id: Number;
+
+    @Column({ name: 'typology' })
     private typology: String;
 
     constructor() {
         super();
+    }
+
+    public getId(): Number {
+        return this.id;
+    }
+
+    public setId(id: Number): void {
+        this.id = id;
+    }
+
+    public getTypology(): String {
+        return this.typology;
+    }
+
+    public setTypology(typology: String): void {
+        this.typology = typology;
     }
 
 }

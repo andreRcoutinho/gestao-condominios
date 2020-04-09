99521ded41a05f346751604fc97d4e43616c3607
coutinho <coutinho.andre@icloud.com>
2020-04-09 19:02:54 +0100

    Navigation Drawer routes

--- frontend/src/components/NavDrawer.vue
+++ frontend/src/components/NavDrawer.vue
@@ -1,21 +1,21 @@
 <template>
 	<v-navigation-drawer app clipped absolute permanent color="secondary" width="333">
 		<v-list flat class="mt-4">
-			<v-list-item-group active-class="font-weight-black">
-				<v-list-item
-					v-for="item in items"
-					:key="item.title"
-					:ripple="{ center: true, class: 'white--text' }"
-				>
-					<v-list-item-icon>
-						<v-icon color="white">{{ item.icon }}</v-icon>
-					</v-list-item-icon>
+			<v-list-item
+				v-for="item in items"
+				:key="item.title"
+				:to="item.route"
+				:ripple="{ center: true, class: 'white--text' }"
+				active-class="font-weight-black"
+			>
+				<v-list-item-icon>
+					<v-icon color="white">{{ item.icon }}</v-icon>
+				</v-list-item-icon>
 
-					<v-list-item-content>
-						<v-list-item-title class="white--text">{{ item.title }}</v-list-item-title>
-					</v-list-item-content>
-				</v-list-item>
-			</v-list-item-group>
+				<v-list-item-content>
+					<v-list-item-title class="white--text">{{ item.title }}</v-list-item-title>
+				</v-list-item-content>
+			</v-list-item>
 		</v-list>
 
 		<!-- slots -->
@@ -32,13 +32,13 @@ export default {
 	data() {
 		return {
 			items: [
-				{ title: 'Home', icon: 'mdi-home-city' },
-				{ title: 'Mapa de Pagamento', icon: 'mdi-clipboard-text' },
-				{ title: 'Gestão do Condomínio', icon: 'mdi-office-building' },
-				{ title: 'Movimentos', icon: 'mdi-currency-usd' },
-				{ title: 'Contactos', icon: 'mdi-card-account-mail' },
-				{ title: 'Gestão de Utilizadores', icon: 'mdi-account-group' },
-				{ title: 'Definições', icon: 'mdi-cog' },
+				{ title: 'Home', icon: 'mdi-home-city', route: '/home' },
+				{ title: 'Mapa de Pagamento', icon: 'mdi-clipboard-text', route: '/about' },
+				{ title: 'Gestão do Condomínio', icon: 'mdi-office-building', route: '' },
+				{ title: 'Movimentos', icon: 'mdi-currency-usd', route: '' },
+				{ title: 'Contactos', icon: 'mdi-card-account-mail', route: '' },
+				{ title: 'Gestão de Utilizadores', icon: 'mdi-account-group', route: '' },
+				{ title: 'Definições', icon: 'mdi-cog', route: '' },
 			],
 		};
 	},

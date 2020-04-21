93bb64c56ca65743e7e755c0b8dd4c4240d1033b
Francisco_Saraiva <francisco_saraiva94@hotmail.com>
2020-04-21 01:41:50 +0100

    Supplier controller

--- backend/src/controllers/supplier.ts
+++ backend/src/controllers/supplier.ts
@@ -6,25 +6,20 @@ import { Supplier } from '../models/supplier';
 import * as supplierRules from '../rules/supplier';
 
 export async function create(req: Request, res: Response) {
-
     if (!supplierRules.createRules(req.body))
         return res.send(new ApiResponse("Create Supplier", "Invalid Body", HttpStatus.BAD_REQUEST, {}));
 
     let supplier: Supplier = await supplierService.create(req.body);
 
-    return res.send(supplier);
-
-    //return res.send(new ApiResponse("All roles", "Success", HttpStatus.OK, roles));
-    //return res.status(HttpStatus.BAD_REQUEST).send(new ApiResponse("All roles", "Request Failed", HttpStatus.BAD_REQUEST, []));
+    return res.send(res.send(new ApiResponse("Create Supplier", "Supplier created.", HttpStatus.CREATED, supplier)));
 }
 
 
 export async function show(req: Request, res: Response) {
-
     let supplier: Supplier = await supplierService.show(Number(req.params.id));
 
-    return res.send(supplier);
+    if (!supplier)
+        return res.send(new ApiResponse("Show Supplier", "Supplier not found", HttpStatus.OK, {}));
 
-    //return res.send(new ApiResponse("All roles", "Success", HttpStatus.OK, roles));
-    //return res.status(HttpStatus.BAD_REQUEST).send(new ApiResponse("All roles", "Request Failed", HttpStatus.BAD_REQUEST, []));
+    return res.send(new ApiResponse("Show Supplier", "Supplier retrived.", HttpStatus.OK, supplier));
 }

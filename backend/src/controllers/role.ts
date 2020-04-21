d438866cd53b5725307d0d6971fed931c0c722a9
Francisco_Saraiva <francisco_saraiva94@hotmail.com>
2020-04-21 12:04:15 +0100

    role message consts

--- backend/src/controllers/role.ts
+++ backend/src/controllers/role.ts
@@ -3,13 +3,18 @@ import * as roleService from '../services/role';
 import { ApiResponse } from '../api/api_response';
 import HttpStatus from "http-status-codes";
 
+//index
+const GET_ROLES_REQUEST: String = 'Get roles';
+const GET_ROLES_SUCCESS: String = 'Retrieved all roles successfully';
+const GET_ROLES_FAIL: String = 'Failed to retrieve all roles';
+
 export async function index(req: Request, res: Response) {
     let response = await roleService.index();
 
     if (response instanceof Error) {
         console.log(response);
-        return res.status(HttpStatus.BAD_REQUEST).send(new ApiResponse("All roles", "All roles Failed", HttpStatus.BAD_REQUEST, {}, response.message));
+        return res.status(HttpStatus.BAD_REQUEST).send(new ApiResponse(GET_ROLES_REQUEST, GET_ROLES_FAIL, HttpStatus.BAD_REQUEST, {}, response.message));
     } else {
-        return res.send(new ApiResponse("All roles", "All roles Success", HttpStatus.OK, response));
+        return res.send(new ApiResponse(GET_ROLES_REQUEST, GET_ROLES_SUCCESS, HttpStatus.OK, response));
     }
 }

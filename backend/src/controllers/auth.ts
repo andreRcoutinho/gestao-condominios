703d6dacd6fe2427af6e7ca83aa148a92bf2f258
Francisco_Saraiva <francisco_saraiva94@hotmail.com>
2020-04-21 11:59:34 +0100

    auth controller string constants

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -1,37 +1,48 @@
 import { Request, Response } from "express";
-import { User } from "../models/user";
 import HttpStatus from "http-status-codes";
 import * as authServices from "../services/auth";
 import { ApiResponse } from '../api/api_response';
 import * as authRules from '../rules/auth';
 
+// Sign up
+const AUTH_SIGN_UP_REQUEST: String = 'Sign up';
+const AUTH_SIGN_UP_MESSAGE_SUCCESS: String = 'Signed up user successfully';
+const AUTH_SIGN_UP_MESSAGE_FAIL: String = 'Failed to sign up user';
+// Sign in
+const AUTH_SIGN_IN_REQUEST: String = 'Sign in';
+const AUTH_SIGN_IN_MESSAGE_SUCCESS: String = 'Signed in user successfully';
+const AUTH_SIGN_IN_MESSAGE_FAIL: String = 'Failed to sign in user';
+
+//Invalid body
+const AUTH_INVALID_BODY: String = 'Invalid JSON body';
+
 export async function signUp(req: Request, res: Response) {
     if (!authRules.signUpRules(req.body)) {
-        return res.status(HttpStatus.BAD_REQUEST).send(new ApiResponse("Sign Up", "Sign Up Failed", HttpStatus.BAD_REQUEST, {}, "Invalid Body"));
+        return res.status(HttpStatus.BAD_REQUEST).send(new ApiResponse(AUTH_SIGN_UP_REQUEST, AUTH_SIGN_UP_MESSAGE_FAIL, HttpStatus.BAD_REQUEST, {}, AUTH_INVALID_BODY));
     }
 
     let response = await authServices.signUp(req.body);
 
     if (response instanceof Error) {
         console.log(response);
-        return res.status(HttpStatus.BAD_REQUEST).send(new ApiResponse("Sign Up", "Sign Up Failed", HttpStatus.BAD_REQUEST, {}, response.message));
+        return res.status(HttpStatus.BAD_REQUEST).send(new ApiResponse(AUTH_SIGN_UP_REQUEST, AUTH_SIGN_UP_MESSAGE_FAIL, HttpStatus.BAD_REQUEST, {}, response.message));
     } else {
-        return res.send(new ApiResponse("Sign Up", "Sign Up Success", HttpStatus.OK, response));
+        return res.send(new ApiResponse(AUTH_SIGN_UP_REQUEST, AUTH_SIGN_UP_MESSAGE_SUCCESS, HttpStatus.OK, response));
     }
 }
 
 export async function signIn(req: Request, res: Response) {
     if (!authRules.signInRules(req.body)) {
-        return res.status(HttpStatus.BAD_REQUEST).send(new ApiResponse("Sign In", "Sign In Failed", HttpStatus.BAD_REQUEST, {}, "Invalid Body"));
+        return res.status(HttpStatus.BAD_REQUEST).send(new ApiResponse(AUTH_SIGN_IN_REQUEST, AUTH_SIGN_IN_MESSAGE_FAIL, HttpStatus.BAD_REQUEST, {}, AUTH_INVALID_BODY));
     }
 
     let response = await authServices.signIn(req.body);
 
     if (response instanceof Error) {
         console.log(response);
-        return res.status(HttpStatus.BAD_REQUEST).send(new ApiResponse("Sign In", "Sign In Failed", HttpStatus.BAD_REQUEST, {}, response.message));
+        return res.status(HttpStatus.BAD_REQUEST).send(new ApiResponse(AUTH_SIGN_IN_REQUEST, AUTH_SIGN_IN_MESSAGE_FAIL, HttpStatus.BAD_REQUEST, {}, response.message));
     } else {
-        return res.send(new ApiResponse("Sign In", "Sign In Success", HttpStatus.OK, response));
+        return res.send(new ApiResponse(AUTH_SIGN_IN_REQUEST, AUTH_SIGN_IN_MESSAGE_SUCCESS, HttpStatus.OK, response));
     }
 }
 

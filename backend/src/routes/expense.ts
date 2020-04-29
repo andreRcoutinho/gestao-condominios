1df884266ed1f8e4885c1cbc2149bca0c7f6915f
coutinho <coutinho.andre@icloud.com>
2020-04-29 02:35:35 +0100

    changed middleware order

--- backend/src/routes/expense.ts
+++ backend/src/routes/expense.ts
@@ -6,10 +6,10 @@ import adminMiddleware from '../middlewares/admin';
 const routes = Router();
 
 //routes.use(authMiddleware);
-//routes.use(adminMiddleware);
-
 routes.get('/', expense.index);
 routes.get('/:id', expense.show);
+
+//routes.use(adminMiddleware);
 routes.put('/:id', expense.update);
 routes.post('/', expense.create);
 routes.delete('/:id', expense.remove);

847ff9d9076dcb75dbf1c8fe11921f6f769a83b0
Francisco_Saraiva <francisco_saraiva94@hotmail.com>
2020-05-30 01:08:37 +0100

    refactored expense and added return values

--- backend/src/controllers/expense.ts
+++ backend/src/controllers/expense.ts
@@ -28,127 +28,55 @@ const EXPENSE_REMOVE_MESSAGE_SUCCESS: String = 'Removed expense successfully';
 const EXPENSE_REMOVE_MESSAGE_FAIL: String = 'Failed to remove expense';
 
 export async function index(req: Request, res: Response) {
-
     let response = await expenseService.index((req.query.year) ? req.query.year.toString() : null);
 
     if (response instanceof Error) {
         return res.status(HttpStatus.BAD_REQUEST).send(new ApiResponse(EXPENSE_INDEX_REQUEST, EXPENSE_INDEX_FAIL, HttpStatus.BAD_REQUEST, {}, response.message));
     } else {
-        return res.send(
-            new ApiResponse(EXPENSE_INDEX_REQUEST, EXPENSE_INDEX_SUCCESS, HttpStatus.OK, response)
-        );
+        return res.send(new ApiResponse(EXPENSE_INDEX_REQUEST, EXPENSE_INDEX_SUCCESS, HttpStatus.OK, response));
     }
 }
 
 export async function show(req: Request, res: Response) {
     let response = await expenseService.show(Number(req.params.id));
+
     if (response instanceof Error) {
-        return res
-            .status(HttpStatus.BAD_REQUEST)
-            .send(
-                new ApiResponse(
-                    EXPENSE_SHOW_REQUEST,
-                    EXPENSE_SHOW_MESSAGE_FAIL,
-                    HttpStatus.BAD_REQUEST,
-                    {},
-                    response.message
-                )
-            );
+        return res.status(HttpStatus.BAD_REQUEST).send(new ApiResponse(EXPENSE_SHOW_REQUEST, EXPENSE_SHOW_MESSAGE_FAIL, HttpStatus.BAD_REQUEST, {}, response.message));
     } else {
-        return res.send(
-            new ApiResponse(
-                EXPENSE_SHOW_REQUEST,
-                EXPENSE_SHOW_MESSAGE_SUCCESS,
-                HttpStatus.OK,
-                response
-            )
-        );
+        return res.send(new ApiResponse(EXPENSE_SHOW_REQUEST, EXPENSE_SHOW_MESSAGE_SUCCESS, HttpStatus.OK, response));
     }
 }
 
 export async function create(req: Request, res: Response) {
     if (!expenseRules.createRules(req.body)) {
-        return res
-            .status(HttpStatus.BAD_REQUEST)
-            .send(
-                new ApiResponse(
-                    EXPENSE_CREATE_REQUEST,
-                    EXPENSE_CREATE_MESSAGE_FAIL,
-                    HttpStatus.BAD_REQUEST,
-                    {},
-                    INVALID_JSON_BODY
-                )
-            );
+        return res.status(HttpStatus.BAD_REQUEST).send(new ApiResponse(EXPENSE_CREATE_REQUEST, EXPENSE_CREATE_MESSAGE_FAIL, HttpStatus.BAD_REQUEST, {}, INVALID_JSON_BODY));
     }
 
     let response = await expenseService.create(req.body);
+
     if (response instanceof Error) {
-        return res
-            .status(HttpStatus.BAD_REQUEST)
-            .send(
-                new ApiResponse(
-                    EXPENSE_CREATE_REQUEST,
-                    EXPENSE_CREATE_MESSAGE_FAIL,
-                    HttpStatus.BAD_REQUEST,
-                    {},
-                    response.message
-                )
-            );
+        return res.status(HttpStatus.BAD_REQUEST).send(new ApiResponse(EXPENSE_CREATE_REQUEST, EXPENSE_CREATE_MESSAGE_FAIL, HttpStatus.BAD_REQUEST, response, response.message));
     } else {
-        return res.send(
-            new ApiResponse(
-                EXPENSE_CREATE_REQUEST,
-                EXPENSE_CREATE_MESSAGE_SUCCESS,
-                HttpStatus.OK,
-                true
-            )
-        );
+        return res.send(new ApiResponse(EXPENSE_CREATE_REQUEST, EXPENSE_CREATE_MESSAGE_SUCCESS, HttpStatus.OK, response));
     }
 }
 
 export async function update(req: Request, res: Response) {
     let response = await expenseService.update(Number(req.params.id), req.body);
+
     if (response instanceof Error) {
-        return res
-            .status(HttpStatus.BAD_REQUEST)
-            .send(
-                new ApiResponse(
-                    EXPENSE_UPDATE_REQUEST,
-                    EXPENSE_UPDATE_FAIL,
-                    HttpStatus.BAD_REQUEST,
-                    {},
-                    response.message
-                )
-            );
+        return res.status(HttpStatus.BAD_REQUEST).send(new ApiResponse(EXPENSE_UPDATE_REQUEST, EXPENSE_UPDATE_FAIL, HttpStatus.BAD_REQUEST, {}, response.message));
     } else {
-        return res.send(
-            new ApiResponse(EXPENSE_UPDATE_REQUEST, EXPENSE_UPDATE_SUCCESS, HttpStatus.OK, true)
-        );
+        return res.send(new ApiResponse(EXPENSE_UPDATE_REQUEST, EXPENSE_UPDATE_SUCCESS, HttpStatus.OK, response));
     }
 }
 
 export async function remove(req: Request, res: Response) {
     let response = await expenseService.remove(Number(req.params.id));
+
     if (response instanceof Error) {
-        return res
-            .status(HttpStatus.BAD_REQUEST)
-            .send(
-                new ApiResponse(
-                    EXPENSE_REMOVE_REQUEST,
-                    EXPENSE_REMOVE_MESSAGE_FAIL,
-                    HttpStatus.BAD_REQUEST,
-                    {},
-                    response.message
-                )
-            );
+        return res.status(HttpStatus.BAD_REQUEST).send(new ApiResponse(EXPENSE_REMOVE_REQUEST, EXPENSE_REMOVE_MESSAGE_FAIL, HttpStatus.BAD_REQUEST, {}, response.message));
     } else {
-        return res.send(
-            new ApiResponse(
-                EXPENSE_REMOVE_REQUEST,
-                EXPENSE_REMOVE_MESSAGE_SUCCESS,
-                HttpStatus.OK,
-                true
-            )
-        );
+        return res.send(new ApiResponse(EXPENSE_REMOVE_REQUEST, EXPENSE_REMOVE_MESSAGE_SUCCESS, HttpStatus.OK, response));
     }
 }

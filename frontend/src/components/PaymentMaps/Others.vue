4e20e087fc468666eb2c7e0fe1461fcca923042a
coutinho <coutinho.andre@icloud.com>
2020-06-15 18:37:04 +0100

    Show Other Maps done

--- frontend/src/components/PaymentMaps/Others.vue
+++ frontend/src/components/PaymentMaps/Others.vue
@@ -133,12 +133,78 @@
 						</v-icon>
 					</template>
 				</v-data-table>
-				<v-dialog v-model="mapDetailedInfo.show" max-width="600px">
-					<!-- TODO - SHOW MORE INFO -->
+				<v-dialog v-model="mapDetailedInfo.show" max-width="850px" height="800px">
 					<v-card>
-						<v-card-title class="ml-2 pt-5"> </v-card-title>
-
+						<v-card-title class="ml-2 pt-5">
+							{{ mapDetailedInfo.payment_map.name }}
+						</v-card-title>
 						<v-card-text>
+							<v-row justify="center" class="my-3">
+								<v-col cols="8">
+									<v-data-table
+										:headers="mapDetailedInfo.headers"
+										:items="mapDetailedInfo.revenues"
+										hide-default-footer
+										class="elevation-1"
+									>
+										<template v-slot:item="props">
+											<td class="text-center">
+												<v-tooltip bottom transition="slide-y-transition">
+													<template v-slot:activator="{ on, attrs }">
+														<v-icon
+															v-if="props.item.paid"
+															color="green"
+															v-bind="attrs"
+															v-on="on"
+														>
+															mdi-check-outline
+														</v-icon>
+														<v-icon v-else color="orange" v-bind="attrs" v-on="on">
+															mdi-emoticon-sad-outline
+														</v-icon>
+													</template>
+													<span> {{ props.item.value }} €</span>
+												</v-tooltip>
+											</td>
+										</template>
+									</v-data-table>
+								</v-col>
+							</v-row>
+
+							<v-row justify="center" class="mb-3">
+								<v-col cols="4" class="text-center">
+									<v-list-item two-line>
+										<v-list-item-content>
+											<v-list-item-subtitle>Descrição</v-list-item-subtitle>
+											<v-list-item-title>
+												{{ mapDetailedInfo.payment_map.description }}
+											</v-list-item-title>
+										</v-list-item-content>
+									</v-list-item>
+								</v-col>
+								<v-col cols="4" class="text-center">
+									<v-list-item two-line>
+										<v-list-item-content>
+											<v-list-item-subtitle>Valor Total</v-list-item-subtitle>
+											<v-list-item-title
+												v-if="mapDetailedInfo.payment_map_values.length > 0"
+											>
+												{{ mapDetailedInfo.payment_map_values[0].value }} €
+											</v-list-item-title>
+										</v-list-item-content>
+									</v-list-item>
+								</v-col>
+								<v-col cols="4" class="text-center">
+									<v-list-item two-line>
+										<v-list-item-content>
+											<v-list-item-subtitle>Criado em</v-list-item-subtitle>
+											<v-list-item-title>{{
+												mapDetailedInfo.payment_map.record_date | formatDate
+											}}</v-list-item-title>
+										</v-list-item-content>
+									</v-list-item>
+								</v-col>
+							</v-row>
 							<v-row class="mr-2">
 								<v-spacer></v-spacer>
 								<v-btn color="blue darken-1" text @click="closeMapInfo">Fechar</v-btn>
@@ -175,7 +241,11 @@ export default {
 		otherMapsToDownload: [],
 
 		mapDetailedInfo: {
+			headers: [],
 			show: false,
+			payment_map: {},
+			payment_map_values: [],
+			revenues: [],
 		},
 		otherMapsTableOptions: {
 			search: '',
@@ -330,11 +400,35 @@ export default {
 				a.click();
 			}
 		},
-		showMapInfo(item) {
-			Object.assign(this.mapDetailedInfo, item);
+		async showMapInfo(item) {
+			let mapAllInfo = null;
+
+			await axios
+				.get(`//localhost:3333/api/payment_map/${item.id}`)
+				.then((res) => {
+					mapAllInfo = res.data.data;
+				})
+				.catch((err) => {
+					console.log(err);
+					mapAllInfo = { error: err };
+				});
+
+			await Object.assign(this.mapDetailedInfo, mapAllInfo);
+
+			this.mapDetailedInfo.revenues.forEach((rev) => {
+				this.mapDetailedInfo.headers.push({
+					text: rev.unit,
+					align: 'center',
+					sortable: false,
+					class: 'black--text',
+				});
+			});
+
+			// console.log(mapAllInfo);
 			this.mapDetailedInfo.show = true;
 		},
 		closeMapInfo() {
+			this.mapDetailedInfo.headers = [];
 			this.mapDetailedInfo.show = false;
 		},
 	},
